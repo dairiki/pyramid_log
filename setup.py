6faa51106737694d5fcb83debea3385f7fd1d00b
Jeff Dairiki <dairiki@dairiki.org>
2014-10-09 12:28:54 -0700

    Release 0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
-version = "0.1.2dev4"
+version = "0.2"
 
 install_requires = [
     'pyramid',
