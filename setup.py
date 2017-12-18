2731813f7d9cff38ab392859212d26c7b64e0698
Jeff Dairiki <dairiki@dairiki.org>
2017-12-17 20:11:39 -0800

    Back to dev

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
-version = "0.2.1"
+version = "0.2.1.post1.dev0"
 
 install_requires = [
     'pyramid',
